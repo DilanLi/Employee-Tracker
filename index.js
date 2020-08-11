147df719a32745b8c331b4e231deacf9b0db57d8
Dilan Li <dilanli0706@gmail.com>
2020-08-11 16:02:53 -0600

    addDepartment(), addRole()

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ connection.query("SELECT title FROM role", function(err, res) {
   }
 });
 
+const employees = [];
+connection.query("SELECT first_name, last_name FROM employee", function(err, res) {
+  if (err) throw err;
+  // console.log(res)
+  for (i=0; i<res.length; i++){
+    employees.push(res[i].first_name + " " + res[i].last_name);
+  }
+});
+
+//construct an object of employee info from database
+// class Employee {
+//   constructor
+// }
+
 //this function asks what the user would like to do and initiates the other functions
 function init() {
   inquirer
@@ -40,19 +54,25 @@ function init() {
       type: "list",
       message: "Welcome to the employee Tracker, what would you like to do?",
       name: "startMenu",
-      choices: ["View employees", "Add an employee", "Update employee role", "Exit"]
+      choices: ["View All employees", "Add an employee", "Update employee role", "Add Department", "Add Role","Exit"]
       }
   ])
   .then( response => {
       switch(response.startMenu) {
-      case "View employees":
+      case "View All employees":
           displayEmployees();
           break;
       case "Add an employee":
           addEmployee();
           break;
       case "Update employee role":
-          updateEmployee();
+          updateEmployeeRole();
+          break;
+      case "Add Department":
+          addDepartment();
+          break;
+      case "Add Role":
+          addRole();
           break;
       case "Exit":
           console.log("Thank you for using the Employee Tracker, have a great day.");
@@ -106,60 +126,109 @@ function addEmployee(){
     }
     ]).then( response => {
         createEmployeeinDatabase(response);
-        init();
       });
 }
 
 function createEmployeeinDatabase(response){
   console.log("   New employee created! \n");
+  let roleId;
+  connection.query(
+    "SELECT * FROM role WHERE title = " + response.role, 
+    function(err, res) {
+      if (err) throw err;
+      console.log(res)
+      roleId = res.id
+    });
   connection.query(
     "INSERT INTO employee SET ?",
     {
       first_name: response.first_name,
       last_name: response.last_name,
-      role: response.role,
+      role_id: roleId,
       manager: response.manager
     },
     function(err, res) {
       if (err) throw err;
       console.log(res.affectedRows + " New employee created!\n");
+      init();
     }
   );
 }
 
-// function updateEmployee(){
-//   const employees = [];
-//   const role_id = [];
-//     inquirer
-//     .prompt([
-//         {
-//         type: "list",
-//         name: "selectedEmployee",
-//         message: "Which employee would you like to update?",
-//         choices: employees
-//         },
-//         {
-//         type: "list",
-//         name: "newRole",
-//         message: "What is the new role for this employee?",
-//         choices: roles
-//         }
-//     ]).then( response => {
-//       console.log("success")
-//         // connection.query(
-//         //   "UPDATE employee SET ? WHERE?", 
-//         //   [
-//         //     {
-//         //       role_id: 
-//         //     },
-//         //     {
-//         //       name: selectedEmployee
-//         //     }
-//         //   ],
-//         //   function(err, res) {
+function updateEmployeeRole(){
+    inquirer
+    .prompt([
+        {
+        type: "list",
+        name: "selectedEmployee",
+        message: "Which employee would you like to update role?",
+        choices: employees
+        },
+        {
+        type: "list",
+        name: "newRole",
+        message: "What is the new role for this employee?",
+        choices: roles
+        }
+    ]).then( response => {
+      employeeNameArray = response.selectedEmployee.split(" ");
+        connection.query(
+          "UPDATE employee SET ? WHERE ?", 
+          [
+            {
+              role: response.newRole
+            },
+            {
+              last_name: employeeNameArray[1]
+            }
+          ],
+          function(err, res) {
+            if (err) throw err;
+            console.log("  Employee role updated! \n");
+            init();      
+        })
+      });  
 
-//         // })
-//       });  
+}
 
-// }
+function addDepartment(){
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "new_department",
+      message: "What department would you like to add?"
+    }
+  ]).then( response => {
+    connection.query("INSERT INTO department SET ?",
+    {
+      name: response.new_department
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + "  New department added! \n");
+      init();
+    })
+  })
+}
 
+function addRole(){
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "new_role",
+      message: "What role would you like to add?"
+    }
+  ]).then( response => {
+    connection.query("INSERT INTO role SET ?",
+    {
+      title: response.new_role
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + "  New role added! \n");
+      init();
+    })
+  })
+}
